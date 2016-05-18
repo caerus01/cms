df34b17838f306281257b66bb47134c8d09da670
Herman Combrink <herman.combrink@gmail.com>
2016-05-18 20:12:04 +0200

    ..

---
+++ Caerus/Database/Startup.Database/Client/Tables/ClientNotes.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ClientNotes](
+	[Id] [uniqueidentifier] NOT NULL,
+	[RefId] [bigint] IDENTITY(1,1) NOT NULL,
+	[ClientRefId] [bigint] NOT NULL,
+	[Title] [nvarchar](100) NOT NULL,
+	[NoteContent] [nvarchar](500) NOT NULL,
+	[DateCreated] DATETIME NOT NULL, 
+    [DateModified] DATETIME NOT NULL, 
+    [UserCreated] NVARCHAR(128) NULL, 
+    [UserModified] NVARCHAR(128) NULL,  
+ CONSTRAINT [IX_DirectClientNotes] UNIQUE NONCLUSTERED 
+(
+	[RefId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
---
+++ Caerus/Database/Startup.Database/Document/Tables/Documents.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Documents](
+	[Id] [uniqueidentifier] NOT NULL,
+	[RefId] [bigint] IDENTITY(1,1) NOT NULL,
+	[OwningEntityType] [int] NOT NULL,
+	[OwningRefId] [bigint] NOT NULL,
+	[DocumentType] [int] NOT NULL,
+	[DocumentPath] [nvarchar](2000) NULL,
+	[OriginalFileName] [varchar](1000) NULL,
+	[ValidationDate] [datetime] NULL,
+	[MimeType] [varchar](500) NULL,
+	[State] [int] NOT NULL,
+	[DateCreated] DATETIME NOT NULL, 
+    [DateModified] DATETIME NOT NULL, 
+    [UserCreated] NVARCHAR(128) NULL, 
+    [UserModified] NVARCHAR(128) NULL,  
+ CONSTRAINT [PK_Documents] PRIMARY KEY CLUSTERED 
+(
+	[RefId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
